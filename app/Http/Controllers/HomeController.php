510f0254781e448b4c003fad2ebd610b4a30cae5
Eclatspad <eclatspad@gmail.com>
2025-06-04 15:40:27 +0100

    new updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    //
+
+    public function dashboard(){
+        $user = Auth::user();
+        if($user->role == 'user'){
+            return redirect('user/dashboard');
+        }else{
+             return redirect('admin/dashboard');
+        }
+    }
+
+    public function userDashboard(){
+        return view('dashboard');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,31 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Livewire\Admin\AdminDashboard;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
+use App\Livewire\User\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-    
+Route::middleware(['auth'])->group(function () {
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+    //  Route::get('user/dashboard', [HomeController::class, 'userDashboard'])
+
+     Route::get('user/dashboard', Dashboard::class)->name('user.dashboard');
+      Route::get('admin/dashboard', AdminDashboard::class)->name('admin.dashboard');
+});
+
+
+
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
